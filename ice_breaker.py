d2612f557b2f3347a8433b637d3115058fd1edb2
Stanly <54090219+stanlys96@users.noreply.github.com>
2026-04-03 13:18:08 +0700

    Add files via upload

--- ice_breaker.py
+++ ice_breaker.py
@@ -4,10 +4,14 @@ from dotenv import load_dotenv
 from langchain_ollama import ChatOllama
 from langchain_core.output_parsers import StrOutputParser
 from third_parties.linkedin import scrape_linkedin_profile
+from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 
 load_dotenv() 
 
-if __name__ == "__main__":
+def ice_break_with(name: str) -> str:
+  linkedin_username = linkedin_lookup_agent(name=name)
+  linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username, mock=True)
+
   summary_template = """
     given the LinkedIn information {information} about a person from I want you to create:
     1. a short summary
@@ -17,11 +21,13 @@ if __name__ == "__main__":
   summary_prompt_template = PromptTemplate(input_variables=["information"], template=summary_template)
 
   llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
-  # llm = ChatOllama(model="llama3")
-  chain = summary_prompt_template | llm | StrOutputParser()
 
-  linkedin_data = scrape_linkedin_profile(linkedin_profile_url="https://www.linkedin.com/in/stanly-sukmajaya", mock=True)
+  chain = summary_prompt_template | llm | StrOutputParser()
 
   res = chain.invoke(input={"information": linkedin_data})
 
   print(res)
+
+if __name__ == "__main__":
+  print("Ice breaker enter")
+  ice_break_with(name="Stanly Sukmajaya Data Scientist")
